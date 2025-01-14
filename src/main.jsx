d08da32eaaa54dac589d580f05c433126411f04f
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2025-01-14 09:37:04 +0100

    HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { Store } from "./redux/app/Store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={Store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>
 );
